8299bd369c14d9eaf939da6056b30c547fe160eb
Matthew Kloecker <matthew.kloecker@gmail.com>
2017-09-28 20:21:27 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -3,7 +3,7 @@
 def valid_move?(board, index)
   if board[index] == "" || board[index] == " " || board[index] == nil && index.between?(0, 8)
     return true
-  end  
+  end
   if board[index] == "X" || board[index] == "O"
     return false
   end
